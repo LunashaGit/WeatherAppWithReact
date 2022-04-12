18a51f6e4e02163899503a4704dacb9bcbd4464c
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-04-12 16:08:51 +0200

    Fix the Name crash Graphic Compare

--- src/GraphCompare.js
+++ src/GraphCompare.js
@@ -18,10 +18,10 @@ const weather = ({weatherdata, weatherdataCompare}) => {
             borderWidth: 2,
             data: [weatherdata.list[0].main.temp, weatherdata.list[8].main.temp, weatherdata.list[16].main.temp, weatherdata.list[24].main.temp, weatherdata.list[32].main.temp]
             },{
-            label: `${weatherdataCompare.city.name}`,
+            label:`${weatherdataCompare.city.name}`,
             fill: false,
             lineTension: 0.5,
-            backgroundColor: 'rgba(75,192,192,1)',
+            backgroundColor: 'rgba(192,75,192,1)',
             borderColor: 'rgba(0,0,0,1)',
             borderWidth: 2,
             data: [weatherdataCompare.list[0].main.temp, weatherdataCompare.list[8].main.temp, weatherdataCompare.list[16].main.temp, weatherdataCompare.list[24].main.temp, weatherdataCompare.list[32].main.temp]
