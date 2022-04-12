2106a8fce499cd9e652dd5c2eabf0684c9250578
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-04-12 14:15:00 +0200

    Add graphic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Weather from './weather';
 import WeatherFiveDays from './weatherFiveDays';
 import { useEffect, useState } from 'react';
+
 function App() {
   const [data, setData] = useState([]);
   const [dataFiveDays, setDataFiveDays] = useState([]);
--- src/weatherFiveDays.js
+++ src/weatherFiveDays.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './weatherFiveDays.scss';
+import {Line} from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 const weather = ({weatherdata}) => {
+    const data = {
+        labels: ['In 1 Day', 'In 2 Days', 'In 3 Days', 'In 4 Days', 'In 5 Days'],
+        datasets: [
+            {
+            label: 'Rainfall',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [weatherdata.list[0].main.temp, weatherdata.list[8].main.temp, weatherdata.list[16].main.temp, weatherdata.list[24].main.temp, weatherdata.list[32].main.temp]
+            }
+        ]
+    }
     const items = [];
     let j = 0;
         for(let i = 0; i < weatherdata.list.length; i += 8, j += 8) {
@@ -18,6 +35,7 @@ const weather = ({weatherdata}) => {
     return(
   <div>
     {items}
+    <Line data={data} />
   </div>
 )}
 
